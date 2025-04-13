96da2e52e1a32c478b273d284fadb756882dad9a
Rob Davis <bfe5e925@opayq.com>
2025-04-13 13:40:01 +0100

    footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-// TODO
+const setup = () => {
+    setCopyrightYear();
+}
+
+const setCopyrightYear = () => {
+    const el = document.getElementById("copyright");
+    const year = new Date().getFullYear();
+    el.innerHTML = year;
+}
+
+// Setup everything
+setup();
