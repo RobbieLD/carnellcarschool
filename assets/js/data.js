007c7783a5544d45d9589ab5482ffa012ac18c69
CarnellCarSchool <carnellcarschool@gmail.com>
2025-10-25 01:20:02 +1000

    Banner date update

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,8 +2,8 @@ const announcements = [
     {
         title: 'Skidpan Day',
         body: 'Fancy a Skidpan day? Sign up to our newsletter to find out when the next one will be',
-        from: '2025-01-01',
-        to: '2025-07-31'
+        from: '2025-10-25',
+        to: '2025-11-30'
     }
 ]
 
