9d427897a71ae2bd906f98e64a64a7a20e85783b
CarnellCarSchool <carnellcarschool@gmail.com>
2025-11-18 22:19:44 +1000

    Next Skidpan Date - 10 Jan

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,7 @@ const announcements = [
     {
         title: 'Skidpan Day',
         body: 'Next Skidpan day - Saturday 10th of January - 12:30pm to 4:30pm',
-        from: '2025-11-18',
+        from: '2025-11-17',
         to: '2026-01-10'
     }
 ]
