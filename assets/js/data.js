a23ba5cc2cd2adbfd07fe17690c9c198482ece53
CarnellCarSchool <carnellcarschool@gmail.com>
2026-07-13 09:00:43 +1000

    Banner date update

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,7 @@ const announcements = [
     {
         title: 'Skidpan Day',
         body: 'The next Skidpan event is on Saturday 25th of July from 5pm to 9pm',
-        from: '2026-07-13',
+        from: '2026-07-14',
         to: '2026-07-25'
     }
 ]
